b6ca4bf389428374172ce6e87e2c70ca5ebc1850
nmunatsibw <mcturnergarai@gmail.com>
2024-03-21 10:03:04 +0200

    refactor(Validations): refactor breast milk crfs

--- flourish_form_validations/form_validators/breast_milk_crf_form_validator.py
+++ flourish_form_validations/form_validators/breast_milk_crf_form_validator.py
@@ -6,7 +6,7 @@ from edc_form_validators import FormValidator, INVALID_ERROR, NOT_REQUIRED_ERROR
 from .crf_form_validator import FormValidatorMixin
 
 
-class BreastMilkCRFFormValidator(FormValidatorMixin, FormValidator):
+class BreastMilkFormValidatorMixin(FormValidatorMixin, FormValidator):
     birth_feeding_vaccine_model = 'flourish_child.birthfeedingvaccine'
 
     @property
@@ -16,142 +16,15 @@ class BreastMilkCRFFormValidator(FormValidatorMixin, FormValidator):
     def onschedule_model_cls(self, onschedule_model):
         return django_apps.get_model(onschedule_model)
 
-    def clean(self):
-        required_fields_exp_mastitis = [
-            'exp_mastitis_count'
-        ]
-
-        responses = ['yes_currently', 'yes_resolved']
-        for field in required_fields_exp_mastitis:
-            self.required_if_true(
-                self.cleaned_data.get('exp_mastitis') in responses,
-                field_required=field,
-            )
-
-        self.required_fields_for_condition("exp_mastitis_count", "mastitis")
-        self.required_fields_for_condition("exp_cracked_nipples_count", "cracked_nipples")
-
-        for x in range(1, 6):
-            self.m2m_other_specify(
-                f'm{x}_{OTHER}',
-                m2m_field=f'mastitis_{x}_action',
-                field_other=f'mastitis_{x}_action_other'
-            )
-            actions = f'mastitis_{x}_action'
-            infection_type = f'mastitis_{x}_type'
-
-            self.validate_infection_type(m2m_field=actions, infection_type=infection_type)
-
-            self.validate_breastfeeding_date(
-                breastfeeding_date=f'mastitis_{x}_date_onset')
-
-            self.m2m_other_specify(
-                f'cn{x}_{OTHER}',
-                m2m_field=f'cracked_nipples_{x}_action',
-                field_other=f'cracked_nipples_{x}_action_other'
-            )
-            actions = f'cracked_nipples_{x}_action'
-            infection_type = f'cracked_nipples_{x}_type'
-
-            self.validate_infection_type(m2m_field=actions, infection_type=infection_type)
-
-            self.validate_breastfeeding_date(
-                breastfeeding_date=f'cracked_nipples_{x}_date_onset')
-
-            self.validate_stopped_breastfed(m2m_field=f'mastitis_{x}_action')
-            self.validate_stopped_breastfed(m2m_field=f'cracked_nipples_{x}_action')
-
-            sbnrq = [
-                f'mastitis_{x}_date_onset',
-                f'mastitis_{x}_type',
-                f'mastitis_{x}_action',
-                f'exp_cracked_nipples',
-                f'milk_collected',
-            ]
-
-            mastitis_action_field = f'mastitis_{x}_action'
-
-            if mastitis_action_field not in sbnrq:
-                for field in sbnrq:
-                    self.m2m_other_specify(
-                        f'm{x}_stopped_breastfeeding',
-                        m2m_field=mastitis_action_field,
-                        field_other=field
-                    )
-
-        self.required_if(
-            NO,
-            field_required='not_collected_reasons',
-            field='milk_collected'
-        )
-
-        milk_collected_field = ['breast_collected', 'milk_collected_volume',
-                                'last_breastfed']
-
-        for field in milk_collected_field:
-            self.required_if(
-                YES,
-                field_required=field,
-                field='milk_collected'
-            )
-
-    def validate_stopped_breastfed(self, m2m_field):
-        qs = self.cleaned_data.get(m2m_field)
-        excluded_fields = ['add_comments']
-        action_key = self.extract_actions(m2m_field)
-        if qs and qs.count() > 0:
-            selected = {obj.short_name: obj.name for obj in qs}
-            if f'{action_key}_stopped_breastfeeding' in selected:
-                fields_after_trigger = False
-                for field_name, field_value in self.cleaned_data.items():
-                    if fields_after_trigger and field_name not in excluded_fields:
-                        if field_value and field_name != m2m_field:
-                            raise ValidationError(
-                                {field_name: 'This field is not required.'}
-                            )
-                    elif field_name == m2m_field:
-                        fields_after_trigger = True
-
-    def validate_breastfeeding_date(self, breastfeeding_date):
-        breastfeeding_date_value = self.cleaned_data.get(breastfeeding_date)
-
-        if not breastfeeding_date_value:
-            return
-
-        maternal_visit = self.cleaned_data.get('maternal_visit')
-        message = None
-        child_subject_identifier = self.get_child_subject_identifier_by_visit(
-            visit=maternal_visit
-        )
-        if child_subject_identifier:
-            infant_feeding = self.get_birth_feeding_vaccine(
-                child_subject_identifier=child_subject_identifier,
-                visit_code='2000D'
-            )
-            if infant_feeding:
-                if infant_feeding.breastfeed_start_dt > breastfeeding_date_value:
-                    message = {
-                        breastfeeding_date: f'Date cannot be before breastfeeding '
-                                            f'initiation date on the infant feeding '
-                                            f'form at birth visit'}
-            else:
-                message = ('could not find birth feeding and vaccine object for child '
-                           f'{child_subject_identifier} at 2000D visit')
-        else:
-            message = 'could not find related child identifier'
-        if message:
-            raise ValidationError(message)
-
     def validate_infection_type(self, m2m_field, infection_type):
 
         infection_type = self.cleaned_data.get(infection_type)
         m2m_field_options = self.cleaned_data.get(m2m_field)
-        m2m_action_key = self.extract_actions(m2m_field)
 
         if m2m_field_options and m2m_field_options.count() > 0:
             selected = {obj.short_name: obj.name for obj in m2m_field_options}
-            if ((infection_type != f'{m2m_action_key}_unilateral' and
-                 f'{m2m_action_key}_uninfected_breast' in selected.keys())):
+            if ((infection_type != 'unilateral' and 'uninfected_breast' in
+                 selected.keys())):
                 message = {
                     m2m_field: 'Breastfed from uninfected breast and pumped and dumped '
                                'from the affected breast” can only be selected if '
@@ -161,8 +34,7 @@ class BreastMilkCRFFormValidator(FormValidatorMixin, FormValidator):
                 raise ValidationError(message, code=NOT_REQUIRED_ERROR)
 
             if m2m_field_options.count() > 1:
-                if (f'{m2m_action_key}_{OTHER}' not in selected.keys() and
-                        m2m_field_options.count() < 3):
+                if OTHER not in selected.keys() and m2m_field_options.count() < 3:
                     message = {
                         m2m_field:
                             f'You can only select more than 1 options if you include '
@@ -179,8 +51,7 @@ class BreastMilkCRFFormValidator(FormValidatorMixin, FormValidator):
                     self._errors.update(message)
                     self._error_codes.append(INVALID_ERROR)
                     raise ValidationError(message, code=INVALID_ERROR)
-                if ((f'{m2m_action_key}both_breasts' and
-                     f'{m2m_action_key}uninfected_breast') in selected.keys()):
+                if ('both_breasts' and 'uninfected_breast') in selected.keys():
                     message = {
                         m2m_field:
                             f'\'Breastfeed from both breasts\' and \'Breastfed from '
@@ -214,58 +85,93 @@ class BreastMilkCRFFormValidator(FormValidatorMixin, FormValidator):
         except self.birth_feeding_vaccine_model_cls.DoesNotExist:
             return None
 
-    def required_repeating_questions(self, condition, field_prefix):
-        return {
-            '1': [f"{field_prefix}_1_date_onset", f"{field_prefix}_1_type",
-                  f"{field_prefix}_1_action"],
-            '2': [f"{field_prefix}_1_date_onset", f"{field_prefix}_1_type",
-                  f"{field_prefix}_1_action",
-                  f"{field_prefix}_2_date_onset", f"{field_prefix}_2_type",
-                  f"{field_prefix}_2_action"],
-            '3': [f"{field_prefix}_1_date_onset", f"{field_prefix}_1_type",
-                  f"{field_prefix}_1_action",
-                  f"{field_prefix}_2_date_onset", f"{field_prefix}_2_type",
-                  f"{field_prefix}_2_action",
-                  f"{field_prefix}_3_date_onset", f"{field_prefix}_3_type",
-                  f"{field_prefix}_3_action"],
-            '4': [f"{field_prefix}_1_date_onset", f"{field_prefix}_1_type",
-                  f"{field_prefix}_1_action",
-                  f"{field_prefix}_2_date_onset", f"{field_prefix}_2_type",
-                  f"{field_prefix}_2_action",
-                  f"{field_prefix}_3_date_onset", f"{field_prefix}_3_type",
-                  f"{field_prefix}_3_action",
-                  f"{field_prefix}_4_date_onset", f"{field_prefix}_4_type",
-                  f"{field_prefix}_4_action"],
-            '5_greater': [f"{field_prefix}_1_date_onset", f"{field_prefix}_1_type",
-                          f"{field_prefix}_1_action",
-                          f"{field_prefix}_2_date_onset", f"{field_prefix}_2_type",
-                          f"{field_prefix}_2_action",
-                          f"{field_prefix}_3_date_onset", f"{field_prefix}_3_type",
-                          f"{field_prefix}_3_action",
-                          f"{field_prefix}_4_date_onset", f"{field_prefix}_4_type",
-                          f"{field_prefix}_4_action",
-                          f"{field_prefix}_5_date_onset", f"{field_prefix}_5_type",
-                          f"{field_prefix}_5_action"],
-        }
+    def validate_breastfeeding_date(self, breastfeeding_date):
+        breastfeeding_date_value = self.cleaned_data.get(breastfeeding_date)
 
-    def required_fields_for_condition(self, condition, field_prefix):
-        fields = self.required_repeating_questions(condition, field_prefix)
-        condition_value = self.cleaned_data.get(condition)
-        if condition_value:
-            for x in range(int(condition_value[0])):
-                condition_fields = fields.get(condition_value)
-                for field in condition_fields:
-                    self.required_if(condition_value,
-                                     field=condition,
-                                     field_required=field)
+        if not breastfeeding_date_value:
+            return
 
-    def extract_actions(self, field):
-        action_key = ''
-        if field.startswith("mastitis_"):
-            action_number = field.split("_")[1]
-            action_key = f"m{action_number}"
-        elif field.startswith("cracked_nipples_"):
-            action_number = field.split("_")[2]
-            action_key = f"cn{action_number}"
+        maternal_visit = self.cleaned_data.get('maternal_visit')
+        message = None
+        child_subject_identifier = self.get_child_subject_identifier_by_visit(
+            visit=maternal_visit
+        )
+        if child_subject_identifier:
+            infant_feeding = self.get_birth_feeding_vaccine(
+                child_subject_identifier=child_subject_identifier,
+                visit_code='2000D'
+            )
+            if infant_feeding:
+                if infant_feeding.breastfeed_start_dt > breastfeeding_date_value:
+                    message = {
+                        breastfeeding_date: f'Date cannot be before breastfeeding '
+                                            f'initiation date on the infant feeding '
+                                            f'form at birth visit'}
+            else:
+                message = ('could not find birth feeding and vaccine object for child '
+                           f'{child_subject_identifier} at 2000D visit')
+        else:
+            message = 'could not find related child identifier'
+        if message:
+            raise ValidationError(message)
 
-        return action_key
+
+class BreastMilkCRFFormValidator(BreastMilkFormValidatorMixin):
+
+    def clean(self):
+        super().clean()
+        responses = ['yes_currently', 'yes_resolved']
+        self.required_if_true(
+            self.cleaned_data.get('exp_mastitis') in responses,
+            field_required='exp_mastitis_count',
+        )
+
+        self.required_if(
+            YES,
+            field_required='exp_cracked_nipples_count',
+            field='exp_cracked_nipples'
+        )
+
+        self.required_if(
+            NO,
+            field_required='not_collected_reasons',
+            field='milk_collected'
+        )
+
+        milk_collected_field = ['breast_collected', 'milk_collected_volume',
+                                'last_breastfed']
+
+        for field in milk_collected_field:
+            self.required_if(
+                YES,
+                field_required=field,
+                field='milk_collected'
+            )
+
+
+class MastitisInlineFormValidator(BreastMilkFormValidatorMixin):
+
+    def clean(self):
+        super().clean()
+        self.validate_breastfeeding_date(breastfeeding_date='mastitis_date_onset')
+        self.m2m_other_specify(
+            OTHER,
+            m2m_field='mastitis_action',
+            field_other='mastitis_action_other'
+        )
+        self.validate_infection_type(
+            m2m_field='mastitis_action', infection_type='mastitis_type')
+
+
+class CrackedNipplesInlineFormValidator(BreastMilkFormValidatorMixin):
+
+    def clean(self):
+        super().clean()
+        self.validate_breastfeeding_date(breastfeeding_date='cracked_nipples_date_onset')
+        self.m2m_other_specify(
+            OTHER,
+            m2m_field='cracked_nipples_action',
+            field_other='cracked_nipples_action_other'
+        )
+        self.validate_infection_type(m2m_field='cracked_nipples_action',
+                                     infection_type='cracked_nipples_type')
--- flourish_form_validations/tests/test_breast_milk_crf_form_validator.py
+++ flourish_form_validations/tests/test_breast_milk_crf_form_validator.py
@@ -8,7 +8,7 @@ from django.test import tag, TestCase
 from edc_base import get_utcnow
 from edc_constants.constants import OTHER
 
-from flourish_form_validations.form_validators import BreastMilkCRFFormValidator
+from flourish_form_validations.form_validators import MastitisInlineFormValidator
 from flourish_form_validations.tests.models import Appointment, BirthFeedingVaccine, \
     FlourishConsentVersion, \
     MaternalVisit, MestitisActions, OnSchedule, RegisteredSubject, Schedule, \
@@ -24,11 +24,11 @@ def onschedule_model_cls(self, onschedule_model):
 class TestBreastMilkCRFFormValidator(TestModeMixin, TestCase):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(BreastMilkCRFFormValidator, *args, **kwargs)
+        super().__init__(MastitisInlineFormValidator, *args, **kwargs)
 
     def setUp(self):
-        BreastMilkCRFFormValidator.onschedule_model_cls = onschedule_model_cls
-        BreastMilkCRFFormValidator.birth_feeding_vaccine_model = \
+        MastitisInlineFormValidator.onschedule_model_cls = onschedule_model_cls
+        MastitisInlineFormValidator.birth_feeding_vaccine_model = \
             'flourish_form_validations.birthfeedingvaccine'
         FlourishConsentVersion.objects.create(
             screening_identifier='ABC12345')
@@ -106,54 +106,19 @@ class TestBreastMilkCRFFormValidator(TestModeMixin, TestCase):
         )
 
         self.cleaned_data = {
-            'mastitis_1_action': self.dummy_actions,
-            'mastitis_2_action': self.dummy_actions,
-            'mastitis_3_action': self.dummy_actions,
-            'mastitis_4_action': self.dummy_actions,
-            'mastitis_5_action': self.dummy_actions,
-            'cracked_nipples_1_action': self.dummy_actions,
-            'cracked_nipples_2_action': self.dummy_actions,
-            'cracked_nipples_3_action': self.dummy_actions,
-            'cracked_nipples_4_action': self.dummy_actions,
-            'cracked_nipples_5_action': self.dummy_actions,
+            'mastitis_action': self.dummy_actions,
+            'cracked_nipples_action': self.dummy_actions,
         }
-
-    def test_validate_stopped_breastfed(self):
-        MestitisActions.objects.get_or_create(
-            short_name='stopped_breastfeeding', name='stopped_breastfeeding'
-        )
-
-        stopped_breastfeeding_action = MestitisActions.objects.filter(
-            short_name='stopped_breastfeeding'
-        )
-
-        self.cleaned_data.update(
-            cracked_nipples_5_action=stopped_breastfeeding_action,
-            milk_collected='some value'
-        )
-
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
-        self.assertRaises(ValidationError, validator.validate)
-        self.assertIn('milk_collected', validator._errors)
 
     def test_validate_breastfeeding_date(self):
         self.cleaned_data.update(
             maternal_visit=self.maternal_visit,
-            mastitis_1_date_onset=datetime.date(2021, 12, 1)
+            mastitis_date_onset=datetime.date(2021, 12, 1)
         )
 
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
+        validator = MastitisInlineFormValidator(cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, validator.validate)
-        self.assertIn('mastitis_1_date_onset', validator._errors)
-
-    def test_validate_stopped_breastfed_not_selected(self):
-        self.cleaned_data['mastitis_1_action'] = self.dummy_actions.exclude(
-            short_name='stopped_breastfeeding')
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
-        validator.clean()
-        self.assertNotIn('mastitis_1_date_onset', validator._errors)
-        self.assertNotIn('mastitis_1_type', validator._errors)
-        self.assertNotIn('mastitis_1_action_other', validator._errors)
+        self.assertIn('mastitis_date_onset', validator._errors)
 
     def test_m2m_other_specify_raises_error_for_missing_other_field(self):
         MestitisActions.objects.get_or_create(
@@ -163,33 +128,11 @@ class TestBreastMilkCRFFormValidator(TestModeMixin, TestCase):
         other_actions = MestitisActions.objects.filter(
             short_name=OTHER
         )
-        self.cleaned_data.update({'mastitis_1_action': other_actions})
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
+        self.cleaned_data.update({'mastitis_action': other_actions})
+        validator = MastitisInlineFormValidator(cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, validator.validate)
-        self.assertIn('mastitis_1_action_other', validator._errors)
+        self.assertIn('mastitis_action_other', validator._errors)
 
-    def test_validate_stopped_breastfeeding_error_with_single_other_field(self):
-        MestitisActions.objects.get_or_create(
-            short_name='stopped_breastfeeding', name='stopped_breastfeeding'
-        )
-
-        stopped_breastfeeding_action = MestitisActions.objects.filter(
-            short_name='stopped_breastfeeding'
-        )
-        self.cleaned_data.update({'mastitis_1_action': stopped_breastfeeding_action,
-                                  'milk_collected': 'some value'})
-        self.cleaned_data.pop('mastitis_2_action')
-        self.cleaned_data.pop('mastitis_3_action')
-        self.cleaned_data.pop('mastitis_4_action')
-        self.cleaned_data.pop('mastitis_5_action')
-        self.cleaned_data.pop('cracked_nipples_1_action')
-        self.cleaned_data.pop('cracked_nipples_2_action')
-        self.cleaned_data.pop('cracked_nipples_3_action')
-        self.cleaned_data.pop('cracked_nipples_4_action')
-        self.cleaned_data.pop('cracked_nipples_5_action')
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
-        self.assertRaises(ValidationError, validator.validate)
-        self.assertIn('milk_collected', validator._errors)
 
     def test_validate_infection_type_multiple_without_other(self):
         MestitisActions.objects.get_or_create(
@@ -200,13 +143,12 @@ class TestBreastMilkCRFFormValidator(TestModeMixin, TestCase):
             short_name='uninfected_breast'
         )
         self.cleaned_data.update({
-            'mastitis_1_action': uninfected_breast_action,
-            'mastitis_1_type': 'some value'
+            'mastitis_action': uninfected_breast_action,
+            'mastitis_type': 'some value'
         })
-        self.cleaned_data.pop('mastitis_2_action')
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
+        validator = MastitisInlineFormValidator(cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, validator.validate)
-        self.assertIn('mastitis_1_action', validator._errors)
+        self.assertIn('mastitis_action', validator._errors)
 
     def test_validate_infection_type_both_breasts_and_uninfected(self):
         MestitisActions.objects.get_or_create(
@@ -220,8 +162,8 @@ class TestBreastMilkCRFFormValidator(TestModeMixin, TestCase):
             Q(short_name='both_breasts') | Q(short_name='uninfected_breast')
         )
         self.cleaned_data.update({
-            'mastitis_1_action': actions
+            'mastitis_action': actions
         })
-        validator = BreastMilkCRFFormValidator(cleaned_data=self.cleaned_data)
+        validator = MastitisInlineFormValidator(cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, validator.validate)
-        self.assertIn('mastitis_1_action', validator._errors)
+        self.assertIn('mastitis_action', validator._errors)
