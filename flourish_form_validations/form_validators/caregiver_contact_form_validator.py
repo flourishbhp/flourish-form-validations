25696761bfa2b3e13b43d148a2138623bb1b98e0
Keba462 <79043038+Keba462@users.noreply.github.com>
2024-04-22 14:35:08 +0200

    refactor

--- flourish_form_validations/form_validators/caregiver_contact_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_contact_form_validator.py
@@ -17,7 +17,8 @@ class CaregiverContactFormValidator(FormValidatorMixin, FormValidator):
         cleaned_data = self.cleaned_data
         self.subject_identifier = self.cleaned_data.get('subject_identifier')
 
-        self.validate_against_consent_datetime(self.cleaned_data.get('report_datetime'))
+        self.validate_against_consent_datetime(
+            self.cleaned_data.get('report_datetime'))
 
         self.required_if(
             're_appointment',
@@ -36,25 +37,25 @@ class CaregiverContactFormValidator(FormValidatorMixin, FormValidator):
             if (cleaned_data.get('contact_type') == 'in_person'
                     and locator.may_visit_home == NO):
                 msg = {'contact_type':
-                           'Caregiver Locator says may visit home: '
-                           f'{locator.may_visit_home}, you cannot make a home visit'
-                           ' to participant if they did not give permission.'}
+                       'Caregiver Locator says may visit home: '
+                       f'{locator.may_visit_home}, you cannot make a home visit'
+                       ' to participant if they did not give permission.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
             if (cleaned_data.get('contact_type') == 'phone_call'
                     and locator.may_call == NO):
                 msg = {'contact_type':
-                           f'Caregiver Locator says may call: {locator.may_call}, '
-                           'you cannot call participant if they did not give permission.'}
+                       f'Caregiver Locator says may call: {locator.may_call}, '
+                       'you cannot call participant if they did not give permission.'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
         else:
             msg = {'__all__':
-                       'Caregiver Locator not found, please add Locator before proceeding.'}
+                   'Caregiver Locator not found, please add Locator before proceeding.'}
             self._errors.update(msg)
             raise ValidationError(msg)
 
-        self.validate_other_specify(field='call_reason')
+        self.validate_other_specify(field='call_outcome_other')
 
         self.required_if(
             YES,
