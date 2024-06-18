1921ae5c9602a4dbddf1b8330d48181234fe0450
Keba462 <motlhabi462@gmail.com>
2024-06-18 12:08:33 +0200

    Refactor

--- flourish_form_validations/form_validators/caregiver_social_work_referral_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_social_work_referral_form_validator.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 
 from .crf_form_validator import FormValidatorMixin
 from .social_work_referral_validator_mixin import SocialWorkReferralValidatorMixin
-from edc_constants.constants import UNKNOWN,UNK
+from edc_constants.constants import UNKNOWN, UNK
 
 class CaregiverSocialWorkReferralFormValidator(
         FormValidatorMixin, SocialWorkReferralValidatorMixin):
@@ -20,6 +20,6 @@ class CaregiverSocialWorkReferralFormValidator(
         if current_hiv_status == UNKNOWN:
             current_hiv_status = UNK
         if current_hiv_status and current_hiv_status != hiv_status:
-                    raise ValidationError({
-                        'current_hiv_status':
-                        f'Participant is {hiv_status}, please correct for current HIV status'})
+            raise ValidationError({
+                'current_hiv_status':
+                f'Participant is {hiv_status}, please correct for current HIV status'})
