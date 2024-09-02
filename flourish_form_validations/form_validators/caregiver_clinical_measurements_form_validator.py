132e523ffc5674e38c67f6ad6765a64334215d82
nmunatsibw <mcturnergarai@gmail.com>
2024-09-02 15:25:29 +0200

    (delete): Remove unused variables from form validator
    Removed the `hip_circ` and `waist_circ` variables from the caregiver clinical measurements form validator. These variables were not used, and their removal helps clean up the code.
    
    Signed-off-by: nmunatsibw <mcturnergarai@gmail.com>

--- flourish_form_validations/form_validators/caregiver_clinical_measurements_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_clinical_measurements_form_validator.py
@@ -60,10 +60,8 @@ class CaregiverClinicalMeasurementsFormValidator(FormValidatorMixin,
         weight_kg = self.cleaned_data.get('weight_kg')
         systolic_bp = self.cleaned_data.get('systolic_bp')
         diastolic_bp = self.cleaned_data.get('diastolic_bp')
-        hip_circ = self.cleaned_data.get('hip_circ')
-        waist_circ = self.cleaned_data.get('waist_circ')
 
-        cm_all = [height, weight_kg, systolic_bp, diastolic_bp, hip_circ, waist_circ]
+        cm_all = [height, weight_kg, systolic_bp, diastolic_bp]
 
         return not any(item is None for item in cm_all)
 
