66a5d9844857596883d72a7e1f3ee747e468e2a1
Kebadiretse Neya <motlhabi462@gmail.com>
2024-12-03 09:30:37 +0200

    Remove duplicate preventative therapy

--- flourish_form_validations/form_validators/caregiver_tb_referral_outcome_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_tb_referral_outcome_form_validator.py
@@ -49,17 +49,11 @@ class CaregiverTBReferralOutcomeFormValidator(FormValidatorMixin, FormValidator)
             self.required_if_true(
                 response in selected,
                 field_required=field)
-
-        tb_preventative_fields = [
-            'tb_preventative_therapy',
-            'tb_isoniazid_preventative_therapy',
-        ]
-
-        for field in tb_preventative_fields:
-            self.required_if(
-                YES,
-                field='tb_treatment',
-                field_required=field
+    
+        self.required_if(
+            YES,
+            field='tb_treatment',
+            field_required='tb_preventative_therapy'
             )
 
         self.validate_other_specify(
@@ -76,7 +70,3 @@ class CaregiverTBReferralOutcomeFormValidator(FormValidatorMixin, FormValidator)
             other_specify_field='other_tb_preventative_therapy'
         )
 
-        self.validate_other_specify(
-            field='tb_isoniazid_preventative_therapy',
-            other_specify_field='other_tb_isoniazid_preventative_therapy'
-        )
