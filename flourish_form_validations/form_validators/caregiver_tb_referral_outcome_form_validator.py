f8e3876cd991062e66967bcb997177b63096bbbb
Ame Diphoko <diphokoame@gmail.com>
2025-04-28 12:19:56 +0200

    update: referral outcomes validations to make sure only 1 outcome exists for a referral

--- flourish_form_validations/form_validators/caregiver_tb_referral_outcome_form_validator.py
+++ flourish_form_validations/form_validators/caregiver_tb_referral_outcome_form_validator.py
@@ -1,12 +1,30 @@
+from django.apps import apps as django_apps
+from django.forms import ValidationError
+from edc_base.utils import get_utcnow
 from edc_constants.constants import NO, OTHER, YES
 from edc_form_validators import FormValidator
-from django.forms import ValidationError
+
+
 from .crf_form_validator import FormValidatorMixin
 
 
 class CaregiverTBReferralOutcomeFormValidator(FormValidatorMixin, FormValidator):
 
+    tb_referral_model = 'flourish_caregiver.tbreferralcaregiver'
+    tb_referral_outcome_model = 'flourish_caregiver.caregivertbreferraloutcome'
+
+    @property
+    def tb_referral_model_cls(self):
+        return django_apps.get_model(self.tb_referral_model)
+
+    @property
+    def tb_referral_outcome_model_cls(self):
+        return django_apps.get_model(self.tb_referral_outcome_model)
+
     def clean(self):
+        super().clean()
+
+        self.validate_outcomes_exists_for_referral()
 
         self.required_if(
                 YES,
@@ -19,7 +37,7 @@ class CaregiverTBReferralOutcomeFormValidator(FormValidatorMixin, FormValidator)
             field='tb_evaluation',
             field_required='reasons',
         )
-        required_fields =['tests_performed','diagnosed_with_tb' ]
+        required_fields = ['tests_performed', 'diagnosed_with_tb', ]
         for field in required_fields:
             self.required_if(
                     YES,
@@ -67,7 +85,7 @@ class CaregiverTBReferralOutcomeFormValidator(FormValidatorMixin, FormValidator)
             self.required_if_true(
                 response in selected,
                 field_required=field)
-    
+
         self.required_if(
             NO,
             field='diagnosed_with_tb',
@@ -97,15 +115,36 @@ class CaregiverTBReferralOutcomeFormValidator(FormValidatorMixin, FormValidator)
                          field='diagnosed_with_tb',
                          field_required='tb_treatment')
         self.validate_results_tb_treatment_and_prevention()
-        
+
     def validate_results_tb_treatment_and_prevention(self):
         tb_treatment = self.cleaned_data.get('tb_treatment')
         diagnosed_with_tb = self.cleaned_data.get('diagnosed_with_tb')
-    
+
         if tb_treatment != YES and diagnosed_with_tb == YES:
-                raise ValidationError({
-                    'tb_treatment': 'If any diagnosed with tb , this field must be Yes',
-                })
+                raise ValidationError(
+                    {'tb_treatment':
+                     'If any diagnosed with tb , this field must be Yes', })
 
-        
+    def validate_outcomes_exists_for_referral(self):
+        """ Check if there's already an outcome completed for the related referral
+        """
+        report_datetime = self.cleaned_data.get('report_datetime', None)
+        try:
+            related_referral = self.tb_referral_model_cls.objects.filter(
+                maternal_visit__subject_identifier=self.subject_identifier,
+                report_datetime__lt=report_datetime).latest('report_datetime')
+        except self.tb_referral_model_cls.DoesNotExist:
+            pass
+        else:
+            referral_dt = getattr(related_referral, 'report_datetime', None)
 
+            referral_outcome = self.tb_referral_outcome_model_cls.objects.filter(
+                maternal_visit__subject_identifier=self.subject_identifier,
+                report_datetime__range=(referral_dt, get_utcnow()))
+            if self.instance:
+                referral_outcome = referral_outcome.exclude(id=self.instance.id)
+            if referral_outcome.exists():
+                visit_code = referral_outcome.first().visit_code
+                raise ValidationError(
+                    {'__all__':
+                     f'Referral outcome already completed at visit {visit_code}'})
